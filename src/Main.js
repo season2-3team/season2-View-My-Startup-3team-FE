995228f8bb5df40eebd8dd940b9fcc5fc75554f0
unknown <d653@naver.com>
2024-09-25 19:04:01 +0900

    Feat: App.js, Nav.js

--- src/Main.js
+++ src/Main.js
@@ -4,15 +4,18 @@ import StartupPage from "./pages/StartupPage";
 import MyComparisonPage from "./pages/MyComparisonPage";
 import ComparisonPage from "./pages/ComparisonPage";
 import InvestmentPage from "./pages/InvestmentPage";
+import App from "./components/App.js";
 
 export default function Main() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartupPage />} />
-        <Route path="/my-comparison" element={<MyComparisonPage />} />
-        <Route path="/comparison" element={<ComparisonPage />} />
-        <Route path="/investment" element={<InvestmentPage />} />
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<StartupPage />} />
+          <Route path="/my-comparison" element={<MyComparisonPage />} />
+          <Route path="/comparison" element={<ComparisonPage />} />
+          <Route path="/investment" element={<InvestmentPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Nav from "./Nav.js";
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}

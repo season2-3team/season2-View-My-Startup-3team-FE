900ca64ddc5d18c91fbe2ccea6fb7207daa45e94
Seo-Yurim <yurim2204@naver.com>
2024-10-01 11:31:07 +0900

    Feat: Container 컴포넌트 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './Nav/Nav.js';
+import Container from './Container.js';
 
 export default function App() {
   return (
     <>
       <Nav />
-      <div style={{ margin: '1.6rem' }}>
+      <Container>
         <Outlet />
-      </div>
+      </Container>
     </>
   );
 }

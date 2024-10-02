82af75f08cb8da910d42af22564498b82c67adb7
unknown <d653@naver.com>
2024-10-02 19:09:09 +0900

    Feat: 모달 MySelection 컴포넌트에 적용

--- src/components/Selection/MySelection.js
+++ src/components/Selection/MySelection.js
@@ -1,20 +1,72 @@
 import styles from './MySelection.module.css';
 import btn_plus from '../../assets/btn_plus.svg';
 import MySelectionModal from './MySelectionModal';
+import { useState } from 'react';
 
 export default function MySelection() {
+  const [isModal, setIsModal] = useState(false);
+  const [selectedStartup, setSelectedStartup] = useState([]);
+
+  const handleOpenModal = () => {
+    setIsModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModal(false);
+  };
+
+  const handleSelectStartup = (startup) => {
+    setSelectedStartup((prev) => [...prev, startup]);
+  };
+
+  const handleRemoveStartup = (id) => {
+    setSelectedStartup((prev) => prev.filter((startup) => startup.id !== id));
+  };
+
   return (
     <div className={styles.section}>
       <h2 className={styles.headerTxt}>나의 기업을 선택해주세요!</h2>
       <div className={styles.borderBox}>
         <div className={styles.innerBox}>
-          <div>
-            <img className={styles.plusBtnImg} src={btn_plus} alt="btn_plus" />
-            <h3>기업 추가</h3>
-          </div>
+          {selectedStartup.map((startup) => (
+            <div key={startup.id} className={styles.selectedStartup}>
+              <img
+                src={startup.image}
+                alt="startupImg"
+                className={styles.startupImg}
+              />
+              <span className={styles.startupName}>{startup.name}</span>
+              <span className={styles.startupCategory}>
+                {startup.category.category}
+              </span>
+              <button
+                className={styles.removeBtn}
+                onClick={() => handleRemoveStartup(startup.id)}
+              >
+                선택 취소
+              </button>
+            </div>
+          ))}
+          {selectedStartup.length === 0 && (
+            <div>
+              <img
+                className={styles.plusBtnImg}
+                src={btn_plus}
+                alt="Add startup"
+                onClick={handleOpenModal}
+              />
+              <h3>기업 추가</h3>
+            </div>
+          )}
         </div>
       </div>
       <button className={styles.compareBtn}>기업 비교하기</button>
+      {isModal && (
+        <MySelectionModal
+          onClose={handleCloseModal}
+          onSelectStartup={handleSelectStartup}
+        />
+      )}
     </div>
   );
 }

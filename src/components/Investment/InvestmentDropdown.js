53d4b26f480e4ca1ae2aa213ae79284723ce2e90
Seo-Yurim <yurim2204@naver.com>
2024-10-02 16:21:32 +0900

    Feat: investment dropdown 메뉴 구현

--- src/components/Investment/InvestmentDropdown.js
+++ src/components/Investment/InvestmentDropdown.js
@@ -1,24 +1,46 @@
 import styles from './InvestmentDropdown.module.css';
+import arrowDown from '../../assets/ic_toggle.svg';
+import Dropdown from '../Dropdown';
+import { useState } from 'react';
+import { useSort } from '../../contexts/SortContext';
 
-const InvestmentDropdown = ({ sortOption, setSortOption }) => {
+export default function InvestmentDropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { sortOption, setSortOption } = useSort();
+
+  const options = [
+    { value: 'invest_amount_desc', label: 'View My Startup 투자 금액 높은 순' },
+    { value: 'invest_amount_asc', label: 'View My Startup 투자 금액 낮은 순' },
+    { value: 'actual_invest_desc', label: '실제 누적 투자 금액 높은 순' },
+    { value: 'actual_invest_asc', label: '실제 누적 투자 금액 낮은 순' }
+  ];
+
+  const handleOptionClick = (val) => {
+    console.log('Seleted sort Option:', val);
+    setSortOption(val);
+    setIsOpen(false);
+  };
+
   return (
-    <div>
-      <select
-        className={styles.menu}
-        value={sortOption}
-        onChange={(e) => setSortOption(e.target.value)}
-      >
-        <option value="invest_amount_desc">
-          View My Startup 투자 금액 높은 순
-        </option>
-        <option value="invest_amount_asc">
-          View My Startup 투자 금액 낮은 순
-        </option>
-        <option value="actual_invest_desc">실제 투자 금액 높은 순</option>
-        <option value="actual_invest_asc">실제 투자 금액 낮은 순</option>
-      </select>
+    <div className={styles.menu}>
+      <div className={styles.selected} onClick={() => setIsOpen(!isOpen)}>
+        {options.find((option) => option.value === sortOption)?.label ||
+          'Select...'}
+        <img className={styles.icon} src={arrowDown} />
+      </div>
+      {isOpen && (
+        <ul className={styles.options}>
+          {options.map((option) => (
+            <li
+              key={option.value}
+              className={styles.option}
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-};
-
-export default InvestmentDropdown;
+}

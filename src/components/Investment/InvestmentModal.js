bbffd47b839cb2dd1cb96df9837e4b3b3e622e26
Seo-Yurim <yurim2204@naver.com>
2024-10-07 16:35:39 +0900

    Feat: 투자하기 모달 유효성 검증 기능 추가

--- src/components/Investment/InvestmentModal.js
+++ src/components/Investment/InvestmentModal.js
@@ -3,10 +3,21 @@ import X from '../../assets/ic_x.svg';
 import visibilityOn from '../../assets/btn_visibility_on.svg';
 import visibilityOff from '../../assets/btn_visibility_off.svg';
 import { useState } from 'react';
+import { createInvestment } from '../../api/InvestmentService';
+import useValidate from '../../hooks/useValidate';
 
 export default function InvestmentModal({ onClose }) {
+  const { values, errors, handleChange, validate, handleBlur } = useValidate({
+    name: '',
+    investAmount: '',
+    comment: '',
+    password: '',
+    checkPassword: ''
+  });
+
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [checkPasswordVisible, setCheckPasswordVisible] = useState(false);
+  const [error, setError] = useState('');
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
@@ -16,10 +27,41 @@ export default function InvestmentModal({ onClose }) {
     setCheckPasswordVisible(!checkPasswordVisible);
   };
 
+  const isInputEmpty = () => {
+    return (
+      values.name.trim() !== '' &&
+      values.investAmount.trim() !== '' &&
+      values.comment.trim() !== '' &&
+      values.password.trim() !== '' &&
+      values.checkPassword.trim() !== ''
+    );
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validate()) {
+      return;
+    }
+
+    try {
+      const investment = { ...values };
+      const res = await createInvestment(investment);
+
+      const investmentID = res.id;
+
+      if (!investmentID) {
+        setError('투자 ID를 얻는 데 실패하였습니다.');
+      }
+    } catch (error) {
+      setError('투자에 실패하였습니다.');
+    }
+  };
+
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div>
             <h1>기업에 투자하기</h1>
             <img
@@ -34,30 +76,69 @@ export default function InvestmentModal({ onClose }) {
             <h1>투자 기업 정보</h1>
             <p>코드잇</p>
           </div>
+
+          {/* 투자자 이름 */}
           <div className={styles.group}>
             <label htmlFor="name">투자자 이름</label>
             <input
               type="text"
               id="name"
               placeholder="투자자 이름을 입력해 주세요"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              style={{
+                border: errors.name
+                  ? '0.1rem solid var(--error-color)'
+                  : '0.1rem solid var(--secondary-gray-200)'
+              }}
             />
+            {errors.name && <div className={styles.error}>{errors.name}</div>}
           </div>
+
+          {/* 투자 금액 */}
           <div className={styles.group}>
             <label htmlFor="investAmount">투자 금액</label>
             <input
               type="text"
               id="investAmount"
               placeholder="투자 금액을 입력해 주세요"
+              value={values.investAmount}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              style={{
+                border: errors.investAmount
+                  ? '0.1rem solid var(--error-color)'
+                  : '0.1rem solid var(--secondary-gray-200)'
+              }}
             />
+            {errors.investAmount && (
+              <div className={styles.error}>{errors.investAmount}</div>
+            )}
           </div>
+
+          {/* 투자 코멘트 */}
           <div className={styles.group}>
             <label htmlFor="comment">투자 코멘트</label>
             <textarea
               type="text"
               id="comment"
               placeholder="투자에 대한 코멘트를 입력해 주세요"
+              value={values.comment}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              style={{
+                border: errors.comment
+                  ? '0.1rem solid var(--error-color)'
+                  : '0.1rem solid var(--secondary-gray-200)'
+              }}
             />
+            {errors.comment && (
+              <div className={styles.error}>{errors.comment}</div>
+            )}
           </div>
+
+          {/* 비밀번호 */}
           <div className={styles.group}>
             <label htmlFor="password">비밀번호</label>
             <div className={styles.password}>
@@ -65,6 +146,14 @@ export default function InvestmentModal({ onClose }) {
                 type={isPasswordVisible ? 'text' : 'password'}
                 id="password"
                 placeholder="비밀번호를 입력해 주세요"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                style={{
+                  border: errors.password
+                    ? '0.1rem solid var(--error-color)'
+                    : '0.1rem solid var(--secondary-gray-200)'
+                }}
               />
               <img
                 src={isPasswordVisible ? visibilityOff : visibilityOn}
@@ -72,7 +161,12 @@ export default function InvestmentModal({ onClose }) {
                 onClick={togglePasswordVisibility}
               />
             </div>
+            {errors.password && (
+              <div className={styles.error}>{errors.password}</div>
+            )}
           </div>
+
+          {/* 비밀번호 확인 */}
           <div className={styles.group}>
             <label htmlFor="checkPassword">비밀번호 확인</label>
             <div className={styles.password}>
@@ -80,6 +174,15 @@ export default function InvestmentModal({ onClose }) {
                 type={checkPasswordVisible ? 'text' : 'password'}
                 id="checkPassword"
                 placeholder="비밀번호를 다시 한 번 입력해 주세요"
+                onClick={togglePasswordVisibility}
+                value={values.checkPassword}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                style={{
+                  border: errors.checkPassword
+                    ? '0.1rem solid var(--error-color)'
+                    : '0.1rem solid var(--secondary-gray-200)'
+                }}
               />
               <img
                 src={checkPasswordVisible ? visibilityOff : visibilityOn}
@@ -87,11 +190,23 @@ export default function InvestmentModal({ onClose }) {
                 onClick={toggleCheckPasswordVisibility}
               />
             </div>
+            {errors.checkPassword && (
+              <div className={styles.error}>{errors.checkPassword}</div>
+            )}
           </div>
           <div className={styles.buttons}>
-            <button onClick={onClose}>취소</button>
-            <button>투자하기</button>
+            <button className={styles.cancel} onClick={onClose}>
+              취소
+            </button>
+            <button
+              className={styles.submit}
+              type="submit"
+              disabled={!isInputEmpty()}
+            >
+              투자하기
+            </button>
           </div>
+          {error && <div className={styles.error}>{error}</div>}
         </form>
       </div>
     </div>

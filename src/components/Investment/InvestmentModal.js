29e7758d92588cd21ed5e4bb3ab78244661fa35b
Seo-Yurim <yurim2204@naver.com>
2024-10-07 17:19:20 +0900

    Feat: Modal 공통 컴포넌트로 변환

--- src/components/Investment/InvestmentModal.js
+++ src/components/Investment/InvestmentModal.js
@@ -5,6 +5,7 @@ import visibilityOff from '../../assets/btn_visibility_off.svg';
 import { useState } from 'react';
 import { createInvestment } from '../../api/InvestmentService';
 import useValidate from '../../hooks/useValidate';
+import Modal from '../Common/Modal';
 
 export default function InvestmentModal({ onClose }) {
   const { values, errors, handleChange, validate, handleBlur } = useValidate({
@@ -59,155 +60,153 @@ export default function InvestmentModal({ onClose }) {
   };
 
   return (
-    <div className={styles.overlay}>
-      <div className={styles.container}>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <div>
-            <h1>기업에 투자하기</h1>
+    <Modal>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div>
+          <h1>기업에 투자하기</h1>
+          <img
+            src={X}
+            onClick={onClose}
+            style={{ cursor: 'pointer' }}
+            alt="close btn"
+          />
+        </div>
+        <div>
+          {/* 임시 하드 코딩 */}
+          <h1>투자 기업 정보</h1>
+          <p>코드잇</p>
+        </div>
+
+        {/* 투자자 이름 */}
+        <div className={styles.group}>
+          <label htmlFor="name">투자자 이름</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="투자자 이름을 입력해 주세요"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            style={{
+              border: errors.name
+                ? '0.1rem solid var(--error-color)'
+                : '0.1rem solid var(--secondary-gray-200)'
+            }}
+          />
+          {errors.name && <div className={styles.error}>{errors.name}</div>}
+        </div>
+
+        {/* 투자 금액 */}
+        <div className={styles.group}>
+          <label htmlFor="investAmount">투자 금액</label>
+          <input
+            type="text"
+            id="investAmount"
+            placeholder="투자 금액을 입력해 주세요"
+            value={values.investAmount}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            style={{
+              border: errors.investAmount
+                ? '0.1rem solid var(--error-color)'
+                : '0.1rem solid var(--secondary-gray-200)'
+            }}
+          />
+          {errors.investAmount && (
+            <div className={styles.error}>{errors.investAmount}</div>
+          )}
+        </div>
+
+        {/* 투자 코멘트 */}
+        <div className={styles.group}>
+          <label htmlFor="comment">투자 코멘트</label>
+          <textarea
+            type="text"
+            id="comment"
+            placeholder="투자에 대한 코멘트를 입력해 주세요"
+            value={values.comment}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            style={{
+              border: errors.comment
+                ? '0.1rem solid var(--error-color)'
+                : '0.1rem solid var(--secondary-gray-200)'
+            }}
+          />
+          {errors.comment && (
+            <div className={styles.error}>{errors.comment}</div>
+          )}
+        </div>
+
+        {/* 비밀번호 */}
+        <div className={styles.group}>
+          <label htmlFor="password">비밀번호</label>
+          <div className={styles.password}>
+            <input
+              type={isPasswordVisible ? 'text' : 'password'}
+              id="password"
+              placeholder="비밀번호를 입력해 주세요"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              style={{
+                border: errors.password
+                  ? '0.1rem solid var(--error-color)'
+                  : '0.1rem solid var(--secondary-gray-200)'
+              }}
+            />
             <img
-              src={X}
-              onClick={onClose}
-              style={{ cursor: 'pointer' }}
-              alt="close btn"
+              src={isPasswordVisible ? visibilityOff : visibilityOn}
+              alt={isPasswordVisible ? '비밀번호 표시' : '비밀번호 숨기기'}
+              onClick={togglePasswordVisibility}
             />
           </div>
-          <div>
-            {/* 임시 하드 코딩 */}
-            <h1>투자 기업 정보</h1>
-            <p>코드잇</p>
-          </div>
+          {errors.password && (
+            <div className={styles.error}>{errors.password}</div>
+          )}
+        </div>
 
-          {/* 투자자 이름 */}
-          <div className={styles.group}>
-            <label htmlFor="name">투자자 이름</label>
+        {/* 비밀번호 확인 */}
+        <div className={styles.group}>
+          <label htmlFor="checkPassword">비밀번호 확인</label>
+          <div className={styles.password}>
             <input
-              type="text"
-              id="name"
-              placeholder="투자자 이름을 입력해 주세요"
-              value={values.name}
+              type={checkPasswordVisible ? 'text' : 'password'}
+              id="checkPassword"
+              placeholder="비밀번호를 다시 한 번 입력해 주세요"
+              value={values.checkPassword}
               onChange={handleChange}
               onBlur={handleBlur}
               style={{
-                border: errors.name
+                border: errors.checkPassword
                   ? '0.1rem solid var(--error-color)'
                   : '0.1rem solid var(--secondary-gray-200)'
               }}
             />
-            {errors.name && <div className={styles.error}>{errors.name}</div>}
-          </div>
-
-          {/* 투자 금액 */}
-          <div className={styles.group}>
-            <label htmlFor="investAmount">투자 금액</label>
-            <input
-              type="text"
-              id="investAmount"
-              placeholder="투자 금액을 입력해 주세요"
-              value={values.investAmount}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              style={{
-                border: errors.investAmount
-                  ? '0.1rem solid var(--error-color)'
-                  : '0.1rem solid var(--secondary-gray-200)'
-              }}
+            <img
+              src={checkPasswordVisible ? visibilityOff : visibilityOn}
+              alt={checkPasswordVisible ? '비밀번호 표시' : '비밀번호 숨기기'}
+              onClick={toggleCheckPasswordVisibility}
             />
-            {errors.investAmount && (
-              <div className={styles.error}>{errors.investAmount}</div>
-            )}
           </div>
-
-          {/* 투자 코멘트 */}
-          <div className={styles.group}>
-            <label htmlFor="comment">투자 코멘트</label>
-            <textarea
-              type="text"
-              id="comment"
-              placeholder="투자에 대한 코멘트를 입력해 주세요"
-              value={values.comment}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              style={{
-                border: errors.comment
-                  ? '0.1rem solid var(--error-color)'
-                  : '0.1rem solid var(--secondary-gray-200)'
-              }}
-            />
-            {errors.comment && (
-              <div className={styles.error}>{errors.comment}</div>
-            )}
-          </div>
-
-          {/* 비밀번호 */}
-          <div className={styles.group}>
-            <label htmlFor="password">비밀번호</label>
-            <div className={styles.password}>
-              <input
-                type={isPasswordVisible ? 'text' : 'password'}
-                id="password"
-                placeholder="비밀번호를 입력해 주세요"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                style={{
-                  border: errors.password
-                    ? '0.1rem solid var(--error-color)'
-                    : '0.1rem solid var(--secondary-gray-200)'
-                }}
-              />
-              <img
-                src={isPasswordVisible ? visibilityOff : visibilityOn}
-                alt={isPasswordVisible ? '비밀번호 표시' : '비밀번호 숨기기'}
-                onClick={togglePasswordVisibility}
-              />
-            </div>
-            {errors.password && (
-              <div className={styles.error}>{errors.password}</div>
-            )}
-          </div>
-
-          {/* 비밀번호 확인 */}
-          <div className={styles.group}>
-            <label htmlFor="checkPassword">비밀번호 확인</label>
-            <div className={styles.password}>
-              <input
-                type={checkPasswordVisible ? 'text' : 'password'}
-                id="checkPassword"
-                placeholder="비밀번호를 다시 한 번 입력해 주세요"
-                value={values.checkPassword}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                style={{
-                  border: errors.checkPassword
-                    ? '0.1rem solid var(--error-color)'
-                    : '0.1rem solid var(--secondary-gray-200)'
-                }}
-              />
-              <img
-                src={checkPasswordVisible ? visibilityOff : visibilityOn}
-                alt={checkPasswordVisible ? '비밀번호 표시' : '비밀번호 숨기기'}
-                onClick={toggleCheckPasswordVisibility}
-              />
-            </div>
-            {errors.checkPassword && (
-              <div className={styles.error}>{errors.checkPassword}</div>
-            )}
-          </div>
-          <div className={styles.buttons}>
-            <button className={styles.cancel} onClick={onClose}>
-              취소
-            </button>
-            <button
-              className={styles.submit}
-              type="submit"
-              disabled={!isInputEmpty()}
-            >
-              투자하기
-            </button>
-          </div>
-          {error && <div className={styles.error}>{error}</div>}
-        </form>
-      </div>
-    </div>
+          {errors.checkPassword && (
+            <div className={styles.error}>{errors.checkPassword}</div>
+          )}
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.cancel} onClick={onClose}>
+            취소
+          </button>
+          <button
+            className={styles.submit}
+            type="submit"
+            disabled={!isInputEmpty()}
+          >
+            투자하기
+          </button>
+        </div>
+        {error && <div className={styles.error}>{error}</div>}
+      </form>
+    </Modal>
   );
 }

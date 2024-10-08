082677c93a9bd9c26f631eeccda2003d4a9fa8d0
JiwooFS <wldndhkd1234@naver.com>
2024-10-08 13:11:07 +0900

    feat: 비교 현황 페이지 컴포넌트 폴더 추가

---
+++ src/components/ComparisonStatus/StatusDropdown.js
@@ -0,0 +1,63 @@
+import styles from './StatusDropdown.module.css';
+import arrowDown from '../../assets/ic_toggle.svg';
+import { useEffect, useRef, useState } from 'react';
+//import { useSort } from '../../contexts/SortContext';
+
+export default function StatusDropdown({ setSortOption }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('my_selection_count_desc');
+  const dropdownRef = useRef(null);
+
+  // 메뉴 외부 클릭 감지
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    // 마우스 클릭 이벤트 리스너 추가
+    document.addEventListener('mousedown', handleClickOutside);
+    
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  const options = [
+    { value: 'my_selection_count_desc', label: '나의 기업 선택 횟수 높은순' },
+    { value: 'my_selection_count_asc', label: '나의 기업 선택 횟수 낮은순' },
+    { value: 'compare_selection_count_desc', label: '비교 기업 선택 횟수 높은순' },
+    { value: 'compare_selection_count_asc', label: '비교 기업 선택 횟수 낮은순' },
+  ];
+
+  const handleOptionClick = (value) => {
+    setSelectedOption(value); // selectedOption 상태 업데이트
+    setSortOption(value); // 부모 컴포넌트로 선택된 옵션 전달
+    setIsOpen(false);
+  };
+
+  return (
+    <div ref={dropdownRef} className={styles.menu}>
+      <div className={styles.selected} onClick={() => setIsOpen(!isOpen)}>
+        {options.find((option) => option.value === selectedOption)?.label ||
+          'Select...'}
+        <img className={styles.icon} src={arrowDown} alt="드롭다운 아이콘" />
+      </div>
+      {isOpen && (
+        <ul className={styles.options}>
+          {options.map((option) => (
+            <li
+              key={option.value}
+              className={styles.option}
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
---
+++ src/components/ComparisonStatus/StatusList.js
@@ -0,0 +1,132 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import styles from './StatusList.module.css';
+import useFetchStartups from '../../hooks/useFetchStartups';
+import Pagination from '../Common/Pagination';
+import noImageIcon from '../../assets/no-image.png';
+import { formatAmount } from '../../utils/formatAmount';
+import StartupHeader from './StatusHeader';
+
+const MAX_ITEMS = 10;
+
+export default function StartupList() {
+  const maxItems = MAX_ITEMS;
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [sortOption, setSortOption] = useState('my_selection_count_desc'); // 기본 정렬 옵션 추가
+  const [currentPage, setCurrentPage] = useState(1); // currentPage 상태를 여기서 관리
+
+  const {
+    startups,
+    error,
+    setOrder,
+    setSort,
+    totalCount,
+    setSearch,
+    showLoading
+  } = useFetchStartups(currentPage, maxItems, 'my_selection_count', 'desc'); // currentPage를 인자로 전달
+
+  const totalPages = Math.ceil(totalCount / maxItems);
+
+  useEffect(() => {
+    setSearch(searchKeyword);
+  }, [searchKeyword, setSearch]);
+
+  useEffect(() => {
+    // 마지막 언더바를 기준으로 나누기 위해 정규식 사용
+    const [order, sort] = sortOption.split(/_(?=[^_]*$)/);
+    setOrder(order);
+    setSort(sort);
+  }, [sortOption, setOrder, setSort]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  if (error) {
+    return <div className="error-message">{error}</div>;
+  }
+
+  return (
+    <>
+      <StatusHeader setSortOption={setSort} />
+
+      <div style={{ width: '100%', overflowX: 'auto' }}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th style={{ width: '6.8rem' }}>순위</th>
+              <th style={{ width: '21.3rem' }}>기업명</th>
+              <th style={{ width: '30.4rem' }}>기업소개</th>
+              <th style={{ width: '15.4rem' }}>카테고리</th>
+              <th style={{ width: '15.4rem' }}>나의 기업 선택 횟수</th>
+              <th style={{ width: '15.4rem' }}>비교 기업 선택 횟수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {showLoading ? (
+              <tr>
+                <td colSpan="7">목록을 불러오는 중입니다....</td>
+              </tr>
+            ) : (
+              startups.map((startup) => (
+                <tr key={startup.id}>
+                  <td>{startup.rank}위</td>
+                  <td style={{ textAlign: 'left' }}>
+                    <Link
+                      to={`/${startup.id}`}
+                      style={{ textDecoration: 'none', color: 'inherit' }}
+                    >
+                      <span
+                        style={{
+                          display: 'inline-block',
+                          verticalAlign: 'middle'
+                        }}
+                      >
+                        <img
+                          src={startup.image || noImageIcon}
+                          alt={`${startup.name} 로고`}
+                          style={{
+                            width: '3.2rem',
+                            height: '3.2rem',
+                            marginRight: '0.8rem',
+                            verticalAlign: 'middle',
+                            borderRadius: '50%',
+                            backgroundColor: 'white',
+                            objectFit: 'cover'
+                          }}
+                        />
+                      </span>
+                      <span
+                        style={{
+                          display: 'inline-block',
+                          verticalAlign: 'middle'
+                        }}
+                      >
+                        {startup.name}
+                      </span>
+                    </Link>
+                  </td>
+                  <td className={styles.description}>{startup.description}</td>
+                  <td>{startup.categoryName}</td>
+                  <td style={{ textAlign: 'center' }}>
+                    {formatAmount(startup.mySelectionCount)}
+                  </td>
+                  <td style={{ textAlign: 'center' }}>
+                    {formatAmount(startup.compareSelectionCount)}
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </>
+  );
+}

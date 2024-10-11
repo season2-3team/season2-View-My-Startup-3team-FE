6dc9dd87fdbc9c4cc28b0e17525ed467f4402d6e
kirinkiri <k.melissa0520@gmail.com>
2024-10-11 09:15:08 +0900

    fix: 기업상세api주소 변경

--- src/api/StartupDetailService.js
+++ src/api/StartupDetailService.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: "https://season2-view-my-startup-3team-be.onrender.com/api/startups",
 });
 
 instance.interceptors.response.use(
   (res) => res,
   (err) => {
     console.log(
-      '주의 : 에러 발생!',
-      err.response ? err.response.data : err.message
+      "주의 : 에러 발생!",
+      err.response ? err.response.data : err.message,
     );
 
     throw err;
-  }
+  },
 );
 
 async function get(url, params = {}) {
